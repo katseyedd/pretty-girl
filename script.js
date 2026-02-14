fe6ed001e0f13bfc986a8a07dc576eda0d1e022c
Katseyedd <katseyeddbusiness@gmail.com>
2026-02-14 01:01:00 +0100

    mobile text support

--- script.js
+++ script.js
@@ -136,6 +136,12 @@ var moon = {
     opacity: 0.4
 };
 
+// Flowers setup
+var flowersImage = new Image();
+flowersImage.src = "public/flowers.png";
+var flowersOpacity = 0;
+var flowersShowing = false;
+
 var frameNumber = 0;
 var opacity = 0;
 var secondOpacity = 0;
@@ -711,6 +717,42 @@ function drawText() {
      context.shadowOffsetY = 0;
 }
 
+function drawFlowers() {
+    // Start showing flowers after all text is done (frameNumber >= 5250)
+    if (frameNumber >= 5250 && !flowersShowing) {
+        flowersShowing = true;
+    }
+    
+    if (flowersShowing && flowersImage.width > 0 && flowersImage.height > 0) {
+        // Fade in the flowers
+        if (flowersOpacity < 1) {
+            flowersOpacity += 0.015;
+        }
+        
+        // Draw flowers at bottom of screen with fade-in
+        context.save();
+        context.globalAlpha = Math.min(flowersOpacity, 1);
+        
+        // Calculate dimensions to keep aspect ratio
+        var maxWidth = canvas.width * 0.85;
+        var maxHeight = canvas.height * 0.35;
+        var imgAspectRatio = flowersImage.width / flowersImage.height;
+        var displayWidth = maxWidth;
+        var displayHeight = displayWidth / imgAspectRatio;
+        
+        if (displayHeight > maxHeight) {
+            displayHeight = maxHeight;
+            displayWidth = displayHeight * imgAspectRatio;
+        }
+        
+        var x = (canvas.width - displayWidth) / 2;
+        var y = canvas.height - displayHeight - 30;
+        
+        context.drawImage(flowersImage, x, y, displayWidth, displayHeight);
+        context.restore();
+    }
+}
+
 function draw() {
     drawPurpleBackground();
     
@@ -722,6 +764,7 @@ function draw() {
     drawNebula();
     drawPlanets();
     drawSparkles();
+    drawFlowers();
     
     // Only draw text and increment frame if envelope is open
     if (isEnvelopeOpen) {
